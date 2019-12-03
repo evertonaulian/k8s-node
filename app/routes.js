2d98ac9fe690e9d8034b12dec9562ce9ca7b58bc
Everton Andrade <everton.aulian@gmail.com>
2019-12-03 10:18:34 -0300

    changing button message

--- app/routes.js
+++ app/routes.js
@@ -7,7 +7,7 @@ const requestHandler = (req, res) => {
         res.write('<html>');
         res.write('<head><title>Enter Message</title><head>');
         res.write(
-          '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send ...</button></form></body>'
+          '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send Message</button></form></body>'
         );
         res.write('</html>');
         return res.end();
@@ -46,4 +46,3 @@ const requestHandler = (req, res) => {
 
     exports.handler = requestHandler;
     exports.someText = 'Some hard coded text';
-    
